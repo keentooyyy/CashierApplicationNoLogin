fe00f67ccf46d70cabf1c470ec211e576d8a0df9
keentooyyy <kenneth.lobaton.58@gmail.com>
2022-06-16 12:22:21 +0800

    Initial Setup

--- CashierApplication/frmLoginAccount..cs
+++ CashierApplication/frmLoginAccount..cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace UserAccountNamespace
 {
-    public partial class Form1 : Form
+    public partial class frmLoginAccount : Form
     {
-        public Form1()
+        public frmLoginAccount()
         {
             InitializeComponent();
         }
--- CashierApplication/frmPurchaseDiscountedItem.Designer.cs
+++ CashierApplication/frmPurchaseDiscountedItem.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace UserAccountNamespace
+﻿namespace CashierApplication
 {
-    partial class Form1
+    partial class frmPurchaseDiscountedItem
     {
         /// <summary>
         /// Required designer variable.
@@ -31,10 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "frmPurchaseDiscountedItem";
         }
 
         #endregion
     }
 }
-
