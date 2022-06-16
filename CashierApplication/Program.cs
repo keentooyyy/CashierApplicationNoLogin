e5dd6c089f3f6511b9456f9f586144c3701fa9e1
keentooyyy <kenneth.lobaton.58@gmail.com>
2022-06-16 15:00:33 +0800

    No Login Done

--- CashierApplication/Program.cs
+++ CashierApplication/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace UserAccountNamespace
+namespace CashierApplication
 {
     internal static class Program
     {
--- CashierApplication/frmPurchaseDiscountedItem.cs
+++ CashierApplication/frmPurchaseDiscountedItem.cs
@@ -8,13 +8,52 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace UserAccountNamespace
+namespace CashierApplication
 {
     public partial class frmPurchaseDiscountedItem : Form
     {
+        String TotalAmountText;
+        String ChangeAmountText;
         public frmPurchaseDiscountedItem()
         {
             InitializeComponent();
+        }
+
+        private void ComputeButton_Click(object sender, EventArgs e)
+        {
+            useThisMethod(false);
+            TotalAmountLabel.Text = TotalAmountText;
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            useThisMethod(true);
+            ChangeLabel.Text = ChangeAmountText;
+        }
+        private void useThisMethod(bool withpayment)
+        {
+            String name = ItemTextbox.Text;
+            double price = Convert.ToDouble(PriceTextBox.Text);
+            int quantity = Convert.ToInt32(QuantityTextBox.Text);
+            Double discount = Convert.ToDouble(DiscountTextBox.Text);
+            Double payment;
+
+            if (withpayment == true)
+            {
+                payment = Convert.ToDouble(PaymentTexbox.Text);
+            }
+            else
+            {
+                payment = 0;
+            }
+
+
+            DiscountedItem discountedItem = new DiscountedItem(name, price, quantity, discount);
+            discountedItem.setPayment(payment);
+
+            TotalAmountText = discountedItem.getTotalPrice().ToString();
+
+            ChangeAmountText = discountedItem.getChange().ToString();
         }
     }
 }
